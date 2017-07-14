7a47f18eff49da12527f4601a0f00cb3b39e6e2f
esenes <eugenio.senes@gmail.com>
2017-07-14 15:18:32 +0200

    complers_draft

---
+++ compilers.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Check if compilers are installed and install the missing ones
+# C
+gcc --version
+if [ $? == 0]; then
+	echo "gcc is installed"
+else
+	echo "gcc is not installed"
+fi
+
+# still to finish
+# to add:
+# - g++
+# - gfortran
+# - python
+# - java
+
